eced323a0b8abf561fe930fbd62657c2bf5ce9dc
cda94581 <cda94581@gmail.com>
2021-01-10 00:50:59 -0600

    Parity with main

--- Bot/index.js
+++ Bot/index.js
@@ -1,51 +1,36 @@
-// load node.js's file system module
+// Load node.js's file system module, to get command files
 const fs = require('fs');
 
-// require the discord.js module
+// Require the discord.js module
 const Discord = require('discord.js');
 
-// load the prefix and token from the config.json file
+// Load the prefix and token from the config.json file
 const {
     prefix,
     token
 } = require('./config.json');
 
-// create a new Discord client
+// Create a new Discord client
 const client = new Discord.Client();
-
-//Commands
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-/*
-//Triggers
-client.triggers = new Discord.Collection();
-const triggerFiles = fs.readdirSync('./triggers').filter(file => file.endsWith('.js'));
-*/
 
-//Map commands?
+// Adds all command files that are in the "commands" directory and ends with ".js"
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
 
-    // set a new item in the Collection
+    // Set a new item in the Collection
     // with the key as the command name and the value as the exported module
     client.commands.set(command.name, command);
 }
-/*
-//Map triggers?
-for (const file of triggerFiles) {
-    const triggers = require(`./triggers/${file}`);
-
-    // set a new item in the Collection
-    // with the key as the trigger name and the value as the exported module
-    client.triggers.set(triggers.name, triggers);
-}
-*/
 
 const cooldowns = new Discord.Collection();
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+// When the client is ready, run this code
+// This event will only trigger one time after logging in
 client.once('ready', () => {
+    // Tells the console this is ready
     console.log('Ready!');
     // Activities/Statuses
     client.user.setActivity('STIADS👀 | -help', {type: 'WATCHING'});
@@ -54,59 +39,48 @@ client.once('ready', () => {
 
 // Welcome messages
 client.on('guildMemberAdd', member => {
-    member.guild.channels.cache.get('743467180439371889').send(`Welcome ${member}, to ${member.guild.name}. Please be sure to read <#760613526754164777> before chatting with us, thanks! You are member #${member.guild.memberCount}!`);
+    member.guild.channels.cache.get('760613444323115021').send(`Welcome ${member}, to ${member.guild.name}. Please be sure to read <#760613526754164777> before chatting with us, thanks! You are member #${member.guild.memberCount}!`);
+    // Tells the console the user that joined
     console.log(`${member.user.username} joined`);
 });
 
-// Commands listener
-client.on('message', message => {
-    // tests to make sure the command starts with a prefix and wasn't done by a bot, to continue on
+// When a client sends a message
+client.on('message', message => {	
+    // Tests to make sure the command starts with a prefix and wasn't done by a bot, to continue on
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
-    // dynamically execute commands
+    // Dynamically execute commands
     const command = client.commands.get(commandName) ||
         client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
     if (!command) return;
 
-    // if the command is only for guilds/servers and was executed in dms
+    // If the command is only for guilds/servers and was executed in dms
     if (command.guildOnly && message.channel.type === 'dm') {
         return message.channel.send('I can\'t execute that command inside DMs!');
     }
 	
-	// if command needs to have perms
+	// If command needs to have perms to manage messages
 	if (command.perms && !message.member.hasPermission(command.perms)) {
 		return message.channel.send('nou.');
 	}
 	
-    // if command needs certain roles
-    /*
-	if  (command.roles && !message.member.roles.cache.some(role => role.name === command.roles)) { 
-		return message.channel.send('nou.');
-	}
-    */
-    
-    // if command needs certain channel
-    if (command.channels && message.channel.name != command.channels) {
-        return message.channel.send('You can\'t use this command here.');
-    }
-	
-    // if the command has arguments and is not the required length of the arguments
+    // If the command has arguments and is not the required length of the arguments
     if (command.args && !args.length) {
         let reply = `You didn't provide any arguments.`;
 
-        // if it has a specified usage, add the format
+        // If it has a specified usage, add the format
         if (command.usage) {
             reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
         }
 
-        // send error
+        // Send error
         return message.channel.send(reply);
     }
 
-    // cooldowns
+    // Cooldowns
     if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Discord.Collection());
     }
@@ -132,27 +106,6 @@ client.on('message', message => {
     }
 });
 
-/*
-// Triggers listener = WIP
-client.on('message', message => {	
-    // Tests to make sure the trigger wasn't done by a bot
-    if (message.author.bot) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/); //slices message into arguments
-    const triggerName = args.shift().toLowerCase(); //set the var commandName to the arguments, excluding the first, converted to lower case
 
-    // dynamically execute triggers
-    const trigger = client.commands.get(triggerName) ||
-        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(triggerName));
-
-    if (!trigger) return;
-
-    try {
-        trigger.execute(message);
-    } catch (error) {
-        console.error(error);
-    }
-});
-*/
-
-// login to Discord with your app's token
+// Login to Discord with your app's token
 client.login(token);
