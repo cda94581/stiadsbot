fd944108786e5e5898af951c5fe298ab711db93a
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-02-20 12:41:55 -0600

    Close connection

--- Bot/commands/rank.js
+++ Bot/commands/rank.js
@@ -19,13 +19,13 @@ module.exports = {
 			const sql = `SELECT * FROM stiads_xp WHERE id = ${author}`;
 			con.query(sql, (err, rows) => {
 				if (err) throw err;
+				con.end();
 				if (!rows.length) return message.channel.send('Please send some messages to gain xp!');
 				const xp = rows[0].xp;
 				const level = rows[0].level;
 				const xpToLevel = 5 * (level ** 2) + 50 * level + 100;
 				const embed = new Discord.MessageEmbed().setColor('#0077ff').setTitle(message.author.username).setDescription(`Level: ${level}\nXP: ${xp}/${xpToLevel}`);
 				message.channel.send(embed);
-				con.end();
 			});
 		});
 	},
