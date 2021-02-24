3e0bf13720e6df6cf1e3053608c9facaf49b8b9b
cda94581 <cda94581@gmail.com>
2021-02-24 16:31:32 -0600

    Bots don't get reactions

--- Bot/modules/suggestions.js
+++ Bot/modules/suggestions.js
@@ -1,6 +1,7 @@
 const { feedback_channel } = require('../config.json');
 
 module.exports = message => {
+	if (message.author.bot) return;
 	if (message.channel.id == feedback_channel) {
 		message.react('⬆');
 		message.react('⬇');
