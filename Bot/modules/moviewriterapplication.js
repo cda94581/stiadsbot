8bdfa7b26c7a449d7be080859d8a0e2313f4d628
cda94581 <cda94581@gmail.com>
2021-03-18 10:35:19 -0500

    Confirmation message

--- Bot/modules/moviewriterapplication.js
+++ Bot/modules/moviewriterapplication.js
@@ -5,4 +5,5 @@ module.exports = (message, client) => {
 	if (message.channel.type != 'dm' || !message.content.toLowerCase().startsWith('movie writer application:')) return;
 	const applicationEmbed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`New Movie Writer Application: ${message.author.username}#${message.author.discriminator} - ${message.author.id}`);
 	client.channels.cache.find(channel => channel.id == moviewriterapplicationchannel).send(applicationEmbed);
+	message.channel.send('Movie Application Sent!');
 }
