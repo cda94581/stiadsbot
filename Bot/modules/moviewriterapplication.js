f0c55b2179320dc2598adb8ad94fc3a1a792fbef
cda94581 <cda94581@gmail.com>
2021-03-18 10:55:50 -0500

    In case someone has DMs blocked

--- Bot/modules/moviewriterapplication.js
+++ Bot/modules/moviewriterapplication.js
@@ -5,5 +5,7 @@ module.exports = (message, client) => {
 	if (message.channel.type != 'dm' || !message.content.toLowerCase().startsWith('movie writer application:')) return;
 	const applicationEmbed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`New Movie Writer Application: ${message.author.username}#${message.author.discriminator} - ${message.author.id}`).setDescription(message.content);
 	client.channels.cache.find(channel => channel.id == moviewriterapplicationchannel).send(applicationEmbed);
-	message.channel.send('Movie Application Sent!');
+	try {
+		message.channel.send('Movie Application Sent!');
+	} catch {}
 }
