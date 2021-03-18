c4208382bc2544834ddc237249fe6bd6e09ae5f2
cda94581 <cda94581@gmail.com>
2021-03-18 10:44:02 -0500

    Fix typo ;-;

--- Bot/modules/moviewriterapplication.js
+++ Bot/modules/moviewriterapplication.js
@@ -3,7 +3,7 @@ const { moviewriterapplicationchannel } = require('../config.json');
 
 module.exports = (message, client) => {
 	if (message.channel.type != 'dm' || !message.content.toLowerCase().startsWith('movie writer application:')) return;
-	const applicationEmbed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`New Movie Writer Application: ${message.author.username}#${message.author.discriminator} - ${message.author.id}`).setDescription(messsage.content);
+	const applicationEmbed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`New Movie Writer Application: ${message.author.username}#${message.author.discriminator} - ${message.author.id}`).setDescription(message.content);
 	client.channels.cache.find(channel => channel.id == moviewriterapplicationchannel).send(applicationEmbed);
 	message.channel.send('Movie Application Sent!');
 }
