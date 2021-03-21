6cda7ec83503946e73331ae63feaee8b7da4f27d
cda94581 <cda94581@gmail.com>
2021-03-20 19:26:41 -0500

    Get rid of bot spam 😅

--- Bot/modules/modmessaging.js
+++ Bot/modules/modmessaging.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { modmessagingchannel } = require('../config.json');
 
 module.exports = (message, client) => {
-	if (message.channel.type != 'dm') return;
+	if (message.channel.type != 'dm' || message.author.bot) return;
 	const modmessageEmbed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`New Message: ${message.author.username}#${message.author.discriminator} - ${message.author.id}`).setDescription(message.content);
 	client.channels.cache.find(channel => channel.id == modmessagingchannel).send(modmessageEmbed);
 	try {
