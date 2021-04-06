3e2bef446ca1b47fdfd823ae0c11ecbc892f6521
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-04-05 21:18:48 -0500

    const to let

--- stiadsbot/commands/blacklistxp.js
+++ stiadsbot/commands/blacklistxp.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const mysql = require('mysql');
-const { levelinfo } = require('../config.json');
+let { levelinfo } = require('../config.json');
 
 module.exports = {
 	name: 'blacklistxp',
