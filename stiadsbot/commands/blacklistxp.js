2d57a51b80687e8d2e0101d17e2b94d77c00e690
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-04-06 15:57:26 -0500

    fix config.json not found

--- stiadsbot/commands/blacklistxp.js
+++ stiadsbot/commands/blacklistxp.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const mysql = require('mysql');
 const fs = require('fs');
+const path = require('path');
 let file;
 
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
 	aliases: [ 'blxp' ],
 	execute(message, args, client) {
 		if (args.length < 2) return message.channel.send('Please specify to either blacklist a user or a channel, as well as the id, or to list');
-		fs.readFile((__dirname, '../config.json'), 'utf-8', (err, data) => {
+		fs.readFile(path.resolve(__dirname, '../config.json'), 'utf-8', (err, data) => {
 			if (err) throw err;
 			file = JSON.parse(data);
 		});
@@ -30,7 +31,7 @@ module.exports = {
 				message.channel.send(`Blacklisted Channels:\n${file.levelinfo.blacklist}\nBlacklisted Users:\n${file.levelinfo.userblacklist}`);
 				break;
 		}
-		fs.writeFile((__dirname, '../config.json'), JSON.stringify(file), 'utf-8', err => {
+		fs.writeFile(path.resolve(__dirname, '../config.json'), JSON.stringify(file), 'utf-8', err => {
 			if (err) throw err;
 		});
 	},
