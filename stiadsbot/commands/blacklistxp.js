8e936a244b32f1dac71262a3391e10ef122f5395
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-04-06 16:58:03 -0500

    revert back to version that works while i test

--- stiadsbot/commands/blacklistxp.js
+++ stiadsbot/commands/blacklistxp.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const mysql = require('mysql');
-const fs = require('fs');
-const path = require('path');
-let file;
+let { levelinfo } = require('../config.json');
 
 module.exports = {
 	name: 'blacklistxp',
@@ -14,25 +12,10 @@ module.exports = {
 	aliases: [ 'blxp' ],
 	execute(message, args, client) {
 		if (args.length < 2) return message.channel.send('Please specify to either blacklist a user or a channel, as well as the id, or to list');
-		fs.readFile('./config.json', 'utf-8', (err, data) => {
-			if (err) throw err;
-			file = JSON.parse(data);
-		});
 		switch (args[0].toLowerCase()) {
-			case 'channel':
-				file.levelinfo.blacklist.push(args[1]);
-				message.channel.send('Success!');
-				break;
-			case 'user':
-				file.levelinfo.userblacklist.push(args[1]);
-				message.channel.send('Success!');
-				break;
-			case 'list':
-				message.channel.send(`Blacklisted Channels:\n${file.levelinfo.blacklist}\nBlacklisted Users:\n${file.levelinfo.userblacklist}`);
-				break;
+			case 'channel': levelinfo.blacklist.push(args[1]); message.channel.send('Success!'); break;
+			case 'user': levelinfo.userblacklist.push(args[1]); message.channel.send('Success!'); break;
+			case 'list': message.channel.send(`Blacklisted Channels:\n${levelinfo.blacklist}\nBlacklisted Users:\n${levelinfo.userblacklist}`); break;
 		}
-		fs.writeFile('../config.json', JSON.stringify(file), 'utf-8', err => {
-			if (err) throw err;
-		});
 	},
 };
