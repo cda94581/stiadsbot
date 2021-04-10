b6e46fdd36ef6df78d1680377ba626a238626871
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-04-10 07:37:32 -0500

    mod messaging attach files

--- stiadsbot/modules/modmessaging.js
+++ stiadsbot/modules/modmessaging.js
@@ -3,7 +3,11 @@ const { modmessagingchannel } = require('../config.json');
 
 module.exports = (message, client) => {
 	if (message.channel.type != 'dm' || message.author.bot) return;
-	const modmessageEmbed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`New Message: ${message.author.username}#${message.author.discriminator} - ${message.author.id}`).setDescription(message.content);
+	let modmessageEmbed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`New Message: ${message.author.username}#${message.author.discriminator} - ${message.author.id}`).setDescription(message.content);
+	if (message.attachments){
+		const attachments = message.attachments.map(m => m.url);
+		modmessageEmbed.attachFiles(attachments);
+	}
 	client.channels.cache.find(channel => channel.id == modmessagingchannel).send(modmessageEmbed);
 	try {
 		message.channel.send('Message Sent!');
