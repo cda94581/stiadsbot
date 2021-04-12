e69f32394845266ddceaea0b04ced47b3415d3f7
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-04-11 20:29:51 -0500

    Display leaderboard rank

--- awesome_bot/commands/rank.js
+++ awesome_bot/commands/rank.js
@@ -5,8 +5,9 @@ const { levelinfo } = require('../config.json');
 module.exports = {
 	name: 'rank',
 	description: 'Get your level',
+	type: 'leveling',
 	execute(message, args) {
-		let author = {}
+		let author = {};
 		if (message.mentions.users.first()) {
 			author = {
 				id: message.mentions.users.first().id,
@@ -19,7 +20,6 @@ module.exports = {
 			}
 		}
 
-
 		const con = mysql.createConnection({
 			host: levelinfo.mysqlinfo.host,
 			user: levelinfo.mysqlinfo.user,
@@ -29,16 +29,22 @@ module.exports = {
 
 		con.connect(err => {
 			if (err) throw err;
-			const sql = `SELECT * FROM awesome_xp WHERE id = ${author.id}`;
+			let sql = `SELECT * FROM awesome_xp WHERE id = ${author.id}`;
 			con.query(sql, (err, rows) => {
 				if (err) throw err;
-				con.end();
+				sql = `SELECT * FROM awesome_xp ORDER BY level DESC, xp DESC`;
 				if (!rows.length) return message.channel.send('Please send some messages to gain xp!');
 				const xp = rows[0].xp;
 				const level = rows[0].level;
 				const xpToLevel = 5 * (level ** 2) + 50 * level + 100;
-				const embed = new Discord.MessageEmbed().setColor('#0077ff').setTitle(author.name).setDescription(`Level: ${level}\nXP: ${xp}/${xpToLevel}`);
-				message.channel.send(embed);
+				con.query(sql, (err, rows) => {
+					if (err) throw err;
+					con.end();
+					const rank = rows.indexOf(rows.find(row => row.id == author.id)) + 1;
+					const size = rows.length;
+					const embed = new Discord.MessageEmbed().setColor('#0077ff').setTitle(author.name).setDescription(`Level: ${level}\nXP: ${xp}/${xpToLevel}\nRank: ${rank}/${size}`);
+					message.channel.send(embed);
+				});
 			});
 		});
 	},
