67c9f5bbde15878aa2e6c816f2db480ca6afdcbd
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-04-12 21:02:31 -0500

    reaction roles and remove music

--- stiadsbot/index.js
+++ stiadsbot/index.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const Discord = require('discord.js');
 
 // Load the prefix and token from the config.json file
-const {	prefix,	token, welcomechannel } = require('./config.json');
+const {	prefix,	token, welcomechannel, reactionroles } = require('./config.json');
 
 // Create a new Discord client
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['CHANNEL', 'MESSAGE', 'REACTION']});
 client.commands = new Discord.Collection();
 
 // Adds all command files that are in the "commands" directory and ends with ".js"
@@ -23,9 +23,6 @@ for (const file of commandFiles) {
 }
 
 const cooldowns = new Discord.Collection();
-
-const queue = new Map(); // Music
-const ytdl = require('ytdl-core'); // Music
 
 // When the client is ready, run this code
 // This event will only trigger one time after logging in
@@ -57,17 +54,6 @@ client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
-
-	// Music
-	const serverQueue = queue.get(message.guild.id);
-	if (message.content.startsWith(`${prefix}play`)) {
-		return musicPlay(message, serverQueue);
-	} else if (message.content.startsWith(`${prefix}skip`)) {
-		return musicSkip(message, serverQueue);
-	} else if (message.content.startsWith(`${prefix}stop`)) {
-		return musicStop(message, serverQueue);
-	}
-	// Music End
   
 	// Dynamically execute commands
 	const command = client.commands.get(commandName) ||
@@ -124,101 +110,39 @@ client.on('message', async message => {
 	}
 });
 
-
-// Music
-async function musicPlay(message, serverQueue) {
-	const args = message.content.split(" ");
-
-	const voiceChannel = message.member.voice.channel;
-	if (!voiceChannel)
-		return message.channel.send(
-			"You need to be in a voice channel to play music!"
-		);
-	const permissions = voiceChannel.permissionsFor(message.client.user);
-	if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-		return message.channel.send(
-			"I need the permissions to join and speak in your voice channel!"
-		);
-	}
-
-	const songInfo = await ytdl.getInfo(args[1]);
-	const song = {
-		title: songInfo.videoDetails.title,
-		url: songInfo.videoDetails.video_url,
-	};
-
-	if (!serverQueue) {
-		const queueContruct = {
-			textChannel: message.channel,
-			voiceChannel: voiceChannel,
-			connection: null,
-			songs: [],
-			volume: 5,
-			playing: true
-		};
-
-		queue.set(message.guild.id, queueContruct);
-
-		queueContruct.songs.push(song);
-
+client.on('messageReactionAdd', async (messageReaction, user) => {
+	if (messageReaction.partial) {
 		try {
-			var connection = await voiceChannel.join();
-			queueContruct.connection = connection;
-			play(message.guild, queueContruct.songs[0]);
-		} catch (err) {
-			console.log(err);
-			queue.delete(message.guild.id);
-			return message.channel.send(err);
+			await messageReaction.fetch();
+		} catch {
+			return console.error('Something went wrong: ', error);
 		}
-	} else {
-		serverQueue.songs.push(song);
-		return message.channel.send(`${song.title} has been added to the queue!`);
 	}
-}
-
-function musicSkip(message, serverQueue) {
-	if (!message.member.voice.channel)
-		return message.channel.send(
-			"You have to be in a voice channel to stop the music!"
-		);
-	if (!serverQueue)
-		return message.channel.send("There is no song that I could skip!");
-	serverQueue.connection.dispatcher.end();
-}
-
-function musicStop(message, serverQueue) {
-	if (!message.member.voice.channel)
-		return message.channel.send(
-			"You have to be in a voice channel to stop the music!"
-		);
-
-	if (!serverQueue)
-		return message.channel.send("There is no song that I could stop!");
-
-	serverQueue.songs = [];
-	serverQueue.connection.dispatcher.end();
-}
-
-function play(guild, song) {
-	const serverQueue = queue.get(guild.id);
-	if (!song) {
-		serverQueue.voiceChannel.leave();
-		queue.delete(guild.id);
-		return;
+	for (i in reactionroles) {
+		if ((messageReaction.message.channel.id == reactionroles[i].channel) && (messageReaction.message.id == reactionroles[i].message) || (messageReaction.emoji.id == reactionroles[i].emoji)) {
+			const role = messageReaction.message.member.guild.roles.cache.find(role => role.id == reactionroles[i].role);
+			const member = messageReaction.message.member.guild.members.cache.find(member => member.id == user.id);
+			member.roles.add(role);
+		}
 	}
+});
 
-	const dispatcher = serverQueue.connection
-		.play(ytdl(song.url))
-		.on("finish", () => {
-			serverQueue.songs.shift();
-			play(guild, serverQueue.songs[0]);
-		})
-		.on("error", error => console.error(error));
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-	serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
-// Music End
-
+client.on('messageReactionRemove', async (messageReaction, user) => {
+	if (messageReaction.partial) {
+		try {
+			await messageReaction.fetch();
+		} catch {
+			return console.error('Something went wrong: ', error);
+		}
+	}
+	for (i in reactionroles) {
+		if ((messageReaction.message.channel.id == reactionroles[i].channel) && (messageReaction.message.id == reactionroles[i].message) || (messageReaction.emoji.id == reactionroles[i].emoji)) {
+			const role = messageReaction.message.member.guild.roles.cache.find(role => role.id == reactionroles[i].role);
+			const member = messageReaction.message.member.guild.members.cache.find(member => member.id == user.id);
+			member.roles.remove(role);
+		}
+	}
+});
 
 // Login to Discord with your app's token
 client.login(token);
