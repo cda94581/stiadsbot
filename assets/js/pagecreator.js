948f93a2d7ec58426977269a690fca53816a05cd
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-04-24 10:26:25 -0500

    Convert to jekyll

--- assets/js/pagecreator.js
+++ assets/js/pagecreator.js
@@ -1,20 +1,3 @@
-function writeNav(levels) {
-	let pathBack = "";
-	let levelCount = 1;
-	while (levelCount < levels) {
-		pathBack += "../";
-		levelCount++;
-	}
-	document.write(`
-		<a href="../${pathBack}">
-			<img src="../${pathBack}assets/favicon.png" width=50vmin height=50vmin title="Discord Bots" alt="Discord Bots Icon">
-		</a>
-		<h1 class="no_deco_link topnav">
-			<a href="${pathBack}wiki" title="wiki">Wiki</a>
-		</h1>
-	`);
-}
-
 function determineMode() {
 	let element = document.getElementsByTagName('body')[0];
 	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
---
+++ index.md
@@ -0,0 +1,7 @@
+---
+---
+[stiadsbot](/stiadsbot) [awesome_bot](/awesome_bot) [cdabot](/cdabot)
+
+Hello there, this is a Major WIP...
+
+[Github Branch](https://github.com/cda94581/discord_bots/tree/github_pages/alpha) Commit 129
