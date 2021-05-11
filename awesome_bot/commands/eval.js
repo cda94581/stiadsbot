44ee241af098dbff8fcdc350501a111066af4c60
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-05-11 16:58:18 -0500

    Fix slicing

--- awesome_bot/commands/eval.js
+++ awesome_bot/commands/eval.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Executes a JavaScript code, can be helpful for single-use things.',
 	perms: [ 'ADMINISTRATOR' ],
 	execute(message, args, client) {
-		const content = message.content.slice(0, 5);
+		const content = message.content.slice(6,);
 		try {
 			eval(content);
 			message.channel.send(`Successfully execute code: ${content}`);
