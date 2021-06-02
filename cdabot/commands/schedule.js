6e2de601507ffb2ae3291c07a8fa7473fd8aecaa
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-06-02 06:43:21 -0500

    Schedule

--- cdabot/commands/schedule.js
+++ cdabot/commands/schedule.js
@@ -4,6 +4,6 @@ module.exports = {
 	name: 'schedule',
 	description: 'Content Schedule',
 	execute(message, args) {
-		message.channel.send(new Discord.MessageEmbed().setColor('#cc0000').setTitle('Content Schedule').setDescription('New videos [hopefully] Tuesdays at 2:00 CT\nStreams when cda94581 has time to do so'));
+		message.channel.send(new Discord.MessageEmbed().setColor('#cc0000').setTitle('Content Schedule').setDescription('> This schedule is during the summer months. Videos might still be made during rhe school year.\n**Sunday** - Relax & Catchup\n**Monday** - Record, Stream Building/Mining/Add-ons\n**Tuesday** - New Video!\n**Wednesday** - Record, Stream Servers\n**Thursday** - Record, Optional Time for New Video\n**Friday** - Record, Stream Fan Choice\n**Saturday** - Relax & Catchup'));
 	}
 }
