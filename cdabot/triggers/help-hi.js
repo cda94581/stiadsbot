0a5bbd69f1eb35b56c2bbb2a6d3ff82c3a21c9f8
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-06-06 13:20:12 -0500

    Initial commit

---
+++ cdabot/triggers/help-hi.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports = {
+	names: [ 'hi', 'hello', 'hey', 'heyy' ],
+	type: 'exact',
+	channels: config.triggers.helpHi.channels,
+	execute(message) {
+		message.channel.send(new Discord.MessageEmbed().setTitle('Seriously. You don\'t need to greet us to ask a question.').setDescription('Ask your question! By greeting, you\'re just wasting time. Read more: https://www.nohello.com/'));
+	}
+}
