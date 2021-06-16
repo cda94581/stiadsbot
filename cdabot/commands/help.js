06cbd51444f794438065d320e18bba759eb4dbf8
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-06-15 20:39:03 -0500

    Remove console log

--- cdabot/commands/help.js
+++ cdabot/commands/help.js
@@ -28,7 +28,6 @@ module.exports = {
 				let tempData = data[i];
 				data[i] = tempData.slice(0, 2000);
 				data.push(tempData.slice(2000, tempData.length));
-				console.log(data);
 			}
 			message.channel.send(new Discord.MessageEmbed().setColor('#cc0000').setTitle('CdaBot Help').setDescription(data[i]));
 		}
