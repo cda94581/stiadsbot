8a09e6e8294f89623e077df05b7afd7aa5f6697c
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-06-18 20:16:10 -0500

    Initial commit

---
+++ cdabot/commands/ping.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'ping',
+	description: 'Am I alive? And how long does it take me to respond to the server?',
+	execute(message) {
+		message.channel.send(new Discord.MessageEmbed().setColor('#cc0000').setTitle('Pong!').setDescription(`\`${Math.round(message.guild.shard.ping)}\``));
+	}
+}
