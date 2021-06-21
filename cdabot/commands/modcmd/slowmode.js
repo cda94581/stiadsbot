3c29ac9d00bf1cee7837989dde9c6a0c94b63110
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-06-20 21:18:07 -0500

    Initial commit

---
+++ cdabot/commands/modcmd/slowmode.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'slowmode',
+	description: 'Sets a slowmode in a channel.',
+	usage: '<TIME (in seconds)>',
+	perms: [ 'MANAGE_CHANNELS' ],
+	execute(message, args) {
+		message.channel.setRateLimitPerUser(args[0]);
+		message.channel.send(`Slowmode set to ${args[0]} seconds.`);
+		console.log(`> ${Date().toString()}\t-\tSlowmode of ${args[0]} seconds set for #${message.channel.name}`);
+	}
+}
