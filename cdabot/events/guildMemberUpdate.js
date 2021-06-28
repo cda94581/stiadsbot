33047251e145aff6a6a424af0411d8899ab1b5c8
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-06-27 19:30:38 -0500

    fix username#username instead of username#discriminator

--- cdabot/events/guildMemberUpdate.js
+++ cdabot/events/guildMemberUpdate.js
@@ -3,6 +3,6 @@ const { logchannel } = require('../config.json');
 
 module.exports = (oldMember, newMember) => {
 	const desc = `${oldMember}\nI'm lazy to do the other information rn so just check console logs for details.`;
-	oldMember.client.channels.cache.get(logchannel).send(new Discord.MessageEmbed().setColor('#00cccc').setTitle(`Member Updated: ${oldMember.user.username}#${oldMember.user.username}`).setDescription(desc).setTimestamp(Date.now()));
-	console.log(`> ${Date().toString()}\t-\tMember Updated: ${oldMember.user.username}#${oldMember.user.username}:\nOld:\n${oldMember}\n\nNew:\n${newMember}`);
+	oldMember.client.channels.cache.get(logchannel).send(new Discord.MessageEmbed().setColor('#00cccc').setTitle(`Member Updated: ${oldMember.user.username}#${oldMember.user.discriminator}`).setDescription(desc).setTimestamp(Date.now()));
+	console.log(`> ${Date().toString()}\t-\tMember Updated: ${oldMember.user.username}#${oldMember.user.discriminator}:\nOld:\n${oldMember}\n\nNew:\n${newMember}`);
 }
--- cdabot/events/userUpdate.js
+++ cdabot/events/userUpdate.js
@@ -3,6 +3,6 @@ const { logchannel } = require('../config.json');
 
 module.exports = (oldUser, newUser) => {
 	const desc = `${oldUser}\nI'm lazy to do the other information rn so just check console logs for details.`;
-	oldUser.client.channels.cache.get(logchannel).send(new Discord.MessageEmbed().setColor('#00cccc').setTitle(`User Updated: ${oldUser.username}#${oldUser.username}`).setDescription(desc).setTimestamp(Date.now()));
-	console.log(`> ${Date().toString()}\t-\tMember Updated: ${oldUser.username}#${oldUser.username}:\nOld:\n${oldUser}\n\nNew:\n${newUser}`);
+	oldUser.client.channels.cache.get(logchannel).send(new Discord.MessageEmbed().setColor('#00cccc').setTitle(`User Updated: ${oldUser.username}#${oldUser.discriminator}`).setDescription(desc).setTimestamp(Date.now()));
+	console.log(`> ${Date().toString()}\t-\tMember Updated: ${oldUser.username}#${oldUser.discriminator}:\nOld:\n${oldUser}\n\nNew:\n${newUser}`);
 }
