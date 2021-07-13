fa4631990cb5f829385e0dd984eb5a8ddd6b5752
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-07-13 10:22:40 -0500

    Reaction Roles

--- cdabot/events/messageReactionAdd.js
+++ cdabot/events/messageReactionAdd.js
@@ -1,1 +1,18 @@
-module.exports = (messageReaction, user) => {}
+const reactionroles = require('../reactionroles.json');
+
+module.exports = async (messageReaction, user) => {
+	if (messageReaction.partial) {
+		try {
+			await messageReaction.fetch();
+		} catch {
+			return console.error('Something went wrong: ', error);
+		}
+	}
+	for (i in reactionroles) {
+		if ((messageReaction.message.channel.id == reactionroles[i].channel) && (messageReaction.message.id == reactionroles[i].message) && (( messageReaction.emoji.id || messageReaction.emoji.name ) == reactionroles[i].emoji)) {
+			const role = messageReaction.message.member.guild.roles.cache.find(role => role.id == reactionroles[i].role);
+			const member = messageReaction.message.member.guild.members.cache.find(member => member.id == user.id);
+			return member.roles.add(role);
+		}
+	}
+}
