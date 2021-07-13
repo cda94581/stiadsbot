7eaa875b913b60c2550186257d241a255e6f98ff
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-07-13 08:00:42 -0500

    fix a crash because of a typo

--- cdabot/events/userUpdate.js
+++ cdabot/events/userUpdate.js
@@ -4,9 +4,9 @@ module.exports = (oldUser, newUser) => {
 	const index = require('./index');
 
 	let desc = `${oldUser}`;
-	if (oldUser.defaultAvatarURL != newUser.defaultAvatarURL) data += `\n**Avatar Changed** - \`Old\`: ${oldUser.defaultAvatarURL} \`New\`: ${newUser.defaultAvatarURL}`;
-	if (oldUser.discriminator != newUser.discriminator) data += `\n**Discriminator Changed** - \`Old\`: ${oldUser.discriminator} \`New\`: ${newUser.discriminator}`;
-	if (oldUser.username != newUser.username) data += `\n**Username Changed** - \`Old\`: ${oldUser.username} \`New\`: ${newUser.username}`
+	if (oldUser.defaultAvatarURL != newUser.defaultAvatarURL) desc += `\n**Avatar Changed** - \`Old\`: ${oldUser.defaultAvatarURL} \`New\`: ${newUser.defaultAvatarURL}`;
+	if (oldUser.discriminator != newUser.discriminator) desc += `\n**Discriminator Changed** - \`Old\`: ${oldUser.discriminator} \`New\`: ${newUser.discriminator}`;
+	if (oldUser.username != newUser.username) desc += `\n**Username Changed** - \`Old\`: ${oldUser.username} \`New\`: ${newUser.username}`
 
 	index.log(oldUser, new Discord.MessageEmbed().setColor('#00cccc').setTitle(`User Updated: ${oldUser.username}#${oldUser.discriminator}`).setDescription(desc).setTimestamp(Date.now()));
 	console.log(`> ${Date().toString()}\t-\tMember Updated: ${oldUser.username}#${oldUser.discriminator}:\n${desc}`);
