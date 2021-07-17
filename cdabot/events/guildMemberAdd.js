64dcd9f1adfeaf8478c9235f97dfd098799fb068
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-07-17 15:10:02 -0500

    Banned names and account creation date

--- cdabot/events/guildMemberAdd.js
+++ cdabot/events/guildMemberAdd.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { welcomeChannel } = require('../config.json');
+const bannedwords = require('../bannedwords.json');
 
 module.exports = async member => {
 	if (member.partial) {
@@ -11,7 +12,11 @@ module.exports = async member => {
 	}
 	const index = require('./index');
 
-	const desc = `${member.user} - ${member.user.username}#${member.user.discriminator}\n**ID**: ${member.user.id}`;
+	if (member.id != member.guild.ownerID) {
+		if (bannedwords.some(phrase => member.displayName.toLowerCase().includes(phrase))) member.setNickname( 'Name', 'Inappropriate Name' );
+	}
+
+	const desc = `${member.user} - ${member.user.username}#${member.user.discriminator}\n**ID**: ${member.user.id}\n**Account Created**: ${member.user.createdAt.UTC()}`;
 
 	index.log(member, new Discord.MessageEmbed().setColor('#00cccc').setTitle('Member Joined').setDescription(desc).setTimestamp(Date.now()));
 	console.log(`> ${Date().toString()}\t-\tMember Joined: ${desc}`);
