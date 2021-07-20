0affb2b21ce52e8a82aac9a7a85868a2ec341877
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-07-19 19:02:51 -0500

    Move to modcmd

--- cdabot/commands/modcmd/message.js
+++ cdabot/commands/modcmd/message.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
-const { prefix } = require('../config.json');
+const { prefix } = require('../../config.json');
 
 module.exports = {
-	name: 'modmessage',
+	name: 'message',
 	description: 'Mods may use this to privately DM you asking for [additional] information',
 	usage: '<user> <message>',
 	perms: [ 'BAN_MEMBERS', 'KICK_MEMBERS' ],
 	args: true,
 	// roles: [ 'Moderator', 'Helper' ],
 	execute(message, args) {
-		const modmessageEmbed = new Discord.MessageEmbed().setColor('#cc0000').setTitle('Incoming Mod Message').setDescription(message.content.slice(`${prefix}modmessage ${args[0]}`.length));
+		const modmessageEmbed = new Discord.MessageEmbed().setColor('#cc0000').setTitle('Incoming Mod Message').setDescription(message.content.slice(`${prefix}message ${args[0]}`.length));
 		if (message.attachments){
 			const attachments = message.attachments.map(m => m.url);
 			modmessageEmbed.attachFiles(attachments);
