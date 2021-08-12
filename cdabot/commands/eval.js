d54a785e1513df03076a2c0c95c9fb663b2d7ba4
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-08-11 19:46:28 -0500

    Remove console.log()

--- cdabot/commands/eval.js
+++ cdabot/commands/eval.js
@@ -7,7 +7,6 @@ module.exports = {
 	perms: [ 'ADMINISTRATOR' ],
 	execute(message) {
 		const content = message.content.slice(prefix.length + 5,);
-		console.log(content);
 		try {
 			eval(content);
 			message.channel.send(`Successfully executed code: ${content}`).then(sentMsg => {
