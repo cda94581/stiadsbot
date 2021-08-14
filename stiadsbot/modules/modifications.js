d4636c32897d16f1e0ecd5b15b09389eea0f5f6a
cda94581 <76138236+cda94581@users.noreply.github.com>
2021-08-14 07:59:59 -0500

    Fix modification submission

--- stiadsbot/modules/modifications.js
+++ stiadsbot/modules/modifications.js
@@ -6,7 +6,7 @@ module.exports = (message, client) => {
 	if (!modifications.open) return message.channel.send({ content: 'Sorry, Modification submissions are closed at this time' });
 	const attachments = message.attachments.map(m => m.url);
 	const embed = new Discord.MessageEmbed().setColor('#ff0000').setTitle(`Modification Submission - ${message.author.username}#${message.author.discriminator}`).setDescription(message.content);
-	client.channels.cache.find(channel => channel.id == modifications.channel).send({ embeds: embed, files: attachments }).then(sentMsg => sentMsg.react('👍'));
+	client.channels.cache.find(channel => channel.id == modifications.channel).send({ embeds: [ embed ], files: attachments }).then(sentMsg => sentMsg.react('👍'));
 	try {
 		message.channel.send({ content: 'Message Sent!' });
 	} catch {}
